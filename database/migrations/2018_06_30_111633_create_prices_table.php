60a70b09a8de6ff3bb22fe931e978843ac138b73
lirugo <roostik21@gmail.com>
2018-06-30 14:30:03 +0300

    Created Product|Price controller Created Product|Price model Created products|products_prices migrations Make simply auth user

---
+++ database/migrations/2018_06_30_111633_create_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->decimal('price',10,2);
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->enum('currency',['UAH','USD','RUB']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
